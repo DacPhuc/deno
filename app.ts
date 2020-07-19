6f20a84b1b62260e6e069d5bdfad9b6f103128f2
DacPhuc <phuc.nguyendac@hcmut.edu.vn>
2020-07-19 20:05:39 +0700

    "Change Port"

--- app.ts
+++ app.ts
@@ -1,14 +1,14 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts'
-import router from './router.ts'
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import router from "./router.ts";
 
-const env = Deno.env.toObject()
-const HOST = env.HOST || '127.0.0.1'
-const PORT = env.PORT || 8080
+const env = Deno.env.toObject();
+const HOST = env.HOST || "127.0.0.1";
+const PORT = env.PORT || 3000;
 
-const app = new Application()
+const app = new Application();
 
-app.use(router.routes())
-app.use(router.allowedMethods())
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-console.log(`Listening on port ${PORT} ...`)
-await app.listen(`${HOST}:${PORT}`)
+console.log(`Listening on port ${PORT} ...`);
+await app.listen(`${HOST}:${PORT}`);
